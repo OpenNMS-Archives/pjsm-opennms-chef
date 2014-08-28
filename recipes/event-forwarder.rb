59b9987b9674fcf433afbeaa77e1715103749018
Donald Desloge <thedesloge@gmail.com>
2014-08-28 12:00:52 -0400

    add initial event-forwarder recipe

---
+++ recipes/event-forwarder.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: opennms
+# Recipe:: event-forwarder
+#
+# Copyright 2014, The OpenNMS Group, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "opennms-light"
+
+case node['platform_family']
+when "rhel"
+    home_dir = "/opt/opennms"
+end
+
+when "debian"
+    home_dir = "/usr/share/opennms"
+end
+end
+
+{ "reqPush.sh" => "reqPush.sh.erb"}.each do |dest, source|
+  template "#{home_dir}/bin/#{dest}" do
+    source "#{source}"
+    owner "vagrant"
+    group "vagrant"
+    mode  "0750"
+  end
+end
+
+cron_d "reqPush_cron" do
+  action :create
+  minute "0/5"
+  command "#{home_dir}/bin/reqPush.sh"
+end
