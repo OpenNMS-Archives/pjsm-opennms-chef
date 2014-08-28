bfffeb5fb2157202ce6ca32d2ac2fc6834bdcf28
Donald Desloge <thedesloge@gmail.com>
2014-08-28 14:40:34 -0400

    add event-receiver recipe

---
+++ recipes/event-receiver.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: event-receiver
+# Recipe:: default
+#
+# Copyright 2014, The OpenNMS Group, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "opennms-light"
+
+# Install Karaf ActiveMQ dispatcher configuration, try 120 seconds to get a Karaf connection
+execute "Install OpenNMS activemq dispatcher" do
+  command 'sshpass -p admin ssh -o StrictHostKeyChecking=no admin@localhost -p 8101 "features:install opennms-activemq"'
+  retries 60
+  retry_delay 2
+end
+
+# Install Karaf ActiveMQ event forwarder, try 120 seconds to get a Karaf connection
+execute "Install OpenNMS activemq event forwarder" do
+  command 'sshpass -p admin ssh -o StrictHostKeyChecking=no admin@localhost -p 8101 "features:install opennms-activemq-event-receiver"'
+  retries 60
+  retry_delay 2
+end
+
+# Restart OpenNMS
+service "opennms" do
+  action [:restart]
+end
